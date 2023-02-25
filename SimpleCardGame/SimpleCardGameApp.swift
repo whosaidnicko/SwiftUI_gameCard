1917f7e30db999dc3a61440f715d5c968d817033
whosaidnicko <nicu161299@gmail.com>
2023-02-25 22:43:21 +0200

    Initial Commit

---
+++ SimpleCardGame/SimpleCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCardGameApp.swift
+//  SimpleCardGame
+//
+//  Created by Nicolae Chivriga on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
