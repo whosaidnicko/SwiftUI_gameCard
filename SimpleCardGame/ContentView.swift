04396c5413eaa3c06a35cafc119c0bf8b7bf2cad
whosaidnicko <nicu161299@gmail.com>
2023-02-26 03:43:32 +0200

    Finished app

--- SimpleCardGame/ContentView.swift
+++ SimpleCardGame/ContentView.swift
@@ -8,16 +8,105 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var backgroundImage = "background-plain"
+    @State var playerCard = "card11"
+    @State var cpuCard = "card14"
+    @State var playerScore = 0
+    @State  var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            
+            //MARK: Set Background
+            
+            Image(backgroundImage)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                //MARK: Set Logo
+                
+                Image("logo")
+                
+                Spacer()
+                HStack{
+                    //MARK: Set card Player
+                    
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    //MARK: set CPU card
+                    Image(cpuCard)
+                    Spacer()
+                    
+                }
+                Spacer()
+                //MARK: set Button for deal
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("button")
+                }
+                
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        
+                        Text("Player")
+                            .font(.largeTitle)
+                        //MARK: score for Player
+                        Text("\(playerScore)")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        
+                        Text("CPU")
+                            .font(.largeTitle)
+                        //MARK: score for CPU
+                        Text("\(cpuScore)")
+                            .font(.largeTitle)
+                        
+                    }
+                    Spacer()
+                }
+            }
+        } .foregroundColor(.white)
+    }
+    
+    //MARK: logic button action
+    
+    func deal() {
+        let numberRandomPlayer = Int.random(in: 2...14)
+        let numberRandomCPU = Int.random(in: 2...14)
+        
+        playerCard = "card" + String(numberRandomPlayer)
+        cpuCard = "card" + String(numberRandomCPU)
+        
+        if numberRandomPlayer > numberRandomCPU {
+            //MARK: Player Win
+            playerScore += 1
         }
-        .padding()
+        else if numberRandomCPU > numberRandomPlayer{
+            //MARK: CPU Win
+            cpuScore += 1
+            
+        }
+        else {
+            //MARK: Tie
+            backgroundImage = "background-wood-grain"
+        }
+        
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
